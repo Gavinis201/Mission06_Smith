3a38431aa7cda4d5835916adf498638410bdd614
Gavin Smith <gavinjacobsmith@gmail.com>
2025-02-18 23:42:37 -0700

    woo

---
+++ Mission06_Smith/Migrations/20250219064006_initial2.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission06_Smith.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LentTo",
+                table: "Movies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Movies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LentTo",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Movies");
+        }
+    }
+}
