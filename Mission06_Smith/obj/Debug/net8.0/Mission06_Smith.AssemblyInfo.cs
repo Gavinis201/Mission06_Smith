1f6fa4cef12f4b00ba77e5b364eda9fe96b62721
Gavin Smith <gavinjacobsmith@gmail.com>
2025-02-18 23:43:39 -0700

    woo

--- Mission06_Smith/obj/Debug/net8.0/Mission06_Smith.AssemblyInfo.cs
+++ Mission06_Smith/obj/Debug/net8.0/Mission06_Smith.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission06_Smith")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df588d3616deb2c6f6dc5e788a058ab9e4bed478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a38431aa7cda4d5835916adf498638410bdd614")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission06_Smith")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission06_Smith")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
